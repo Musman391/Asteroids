a901356ff5ae8d81c11468c0bcd0b9d7d2683c2f
Muhammad Usman <musman932@hotmail.com>
2024-12-30 01:33:09 -0700

    Created tick to max the frame rate at 60 fps

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -24,6 +27,7 @@ def main():
         # Set the fill of the screen to Black (0, 0, 0)
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
